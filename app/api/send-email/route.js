722036fe8440aea829455133ea6d0ef9a9dd7d15
hermannafesehbuma <bumahermann13@gmail.com>
2025-02-12 15:10:50 -0500

    Update email sending logic for Titan Mail SMTP

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -18,7 +18,7 @@ export async function POST(req) {
 
     // Email content configuration
     const mailOptions = {
-      from: process.env.TITAN_SMTP_USER, // Sender's email (from environment variables)
+      from: email, // Sender's email (from environment variables)
       to: process.env.TITAN_SMTP_USER, // Recipient email (set in environment variables)
       subject: `New Contact Form Submission from ${firstName} ${lastName}`, // Subject of the email
       text: `
